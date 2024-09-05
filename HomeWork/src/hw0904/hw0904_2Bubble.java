61b32e2766d7177c0976387e377dfbdf99fc1d68
DaHnDell <khanman1208@gmail.com>
2024-09-05 10:38:27 +0900

    소스코드

--- HomeWork/src/hw0904/hw0904_2Bubble.java
+++ HomeWork/src/hw0904/hw0904_2Bubble.java
@@ -2,6 +2,8 @@ package hw0904;
 
 import java.util.Arrays;
 import java.util.Scanner;
+import java.lang.reflect.Array;
+
 
 public class hw0904_2Bubble {
 //	public static void main(String[] args) {
@@ -14,38 +16,29 @@ public class hw0904_2Bubble {
 //		
 //		for(int i =0; i < units.length; i++) {
 //			counts[i] = money / units[i];
-//			money = money % units[i];
-//			System.out.println(Arrays.toString(counts));
+//			money = money % units[i]; // money %= units[i];
+//			System.out.print(Arrays.toString(counts)+" ");
 //		}
+	/* for (int i = 0; i < counts.length; i++){
+	 *  System.out.printf("%d 원 짜리가 %d" + (unit[i] >=1000 ? "장" : "개") + "\n", unit[i], counts[i]); //  짧은 형태의 출력문일수록 3항연산자를 고려해 보자.
+	 * 
+	 * }
+	
+	
+	*/
 //	}
 	// 지폐 및 동전 세기 
 	// 초기 연산자 문제의 응용
 		
-		
+		// 어려우면 언제나 리터럴값을 이용해서 값을 분석합니다.
+	// 수식을 만들 때..
 	//{2, 2, 0, 3, 0, 4, 1, 0, 1, 1};
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	
 	
 	// 2. 사용자 ui를 사용해서, 사람 이름을 입력받습니다. Scanner 사용~ 계속 받습니다. 무한으로! while(boolean) 값 , String
 	// 입력 글자 중, q를 입력하면 입력이 종료됩니다.
 	// 사람 이름들을 문자열 배열으로 만들어서 프로그램 종료직전, 확인할 수 있도록 합니다. ~~ 아마 count 값을 이용해서 배열의 길이에 넣고 선언하는 식으로 ..?
-	public static void main(String[] args) {
+//	public static void main(String[] args) {
 //		Scanner scan = new Scanner(System.in);
 //		String nameIn = "";
 ////		boolean flag = true;
@@ -77,15 +70,59 @@ public class hw0904_2Bubble {
 //			
 //		}
 //	}
-		Scanner scan = new Scanner(System.in);
-		String endQ = new String("q");
-		String nameIn = " ";
-		
-		while(nameIn != endQ) {
-			nameIn=scan.nextLine();	
-			System.out.println(nameIn);
-			
+//		Scanner scan = new Scanner(System.in);
+//		String endQ = new String("q");
+//		String nameIn = " ";
+//		
+//		while(nameIn != endQ) {
+//			nameIn=scan.nextLine();	
+//			System.out.println(nameIn);
+//			
+////		}
+//		
+//	}
+//}
+		public static void main(String[] args) {
+			Scanner scan = new Scanner(System.in); //객체 이름은 클래스 이름의 카멜형 첫 소문자를 이용한 표현을 해보자
+			String nameIn = ""; 
+			int count = 0;
+			String[] tmp = null; //배열 이름은 배열 자료형의 복수형(s)를 사용해서 자동완성 시켜주기도 함.
+			String[] putCopy = new String[2];
+
+			while(true) { // 인덱스가 필요할 때는 for 문이 최적값임
+				// for(int i = 0;; i++
+//				while(!(nameIn.equals("q"))) {
+				nameIn = scan.nextLine(); // 콘솔 특성상 nextLine을 사용하는 것이 좋음
+				System.out.println(nameIn + " 을 입력하였습니다.");
+				if (nameIn.equals("q")) {
+					System.out.println("입력했던 값들을 반환합니다.");
+					for(int i = 0; i < count; i++) {
+						System.out.print(putCopy[i]+" ");
+					}
+					break;
+					/*for(String s : strings){
+					 * 	
+					 * }
+					*/
+				}
+				count++;
+				tmp = new String[count];
+				tmp[count-1] = nameIn;
+//				System.out.println("tmp : " + Arrays.toString(tmp));
+//				System.out.println("cnt : " + count);
+				System.arraycopy(tmp, count-1, putCopy, count-1, 1);
+				if(putCopy.length == count) {	
+					tmp = putCopy;
+					putCopy = new String[putCopy.length+2];
+					System.arraycopy(tmp, 0, putCopy, 0, tmp.length);
+				}
+//				System.out.println("cop : " + Arrays.toString(putCopy));
+//				for(int i = 0; i < putCopy.length-1; i++) {
+//					tmp[i]=putCopy[i];
+//				}
+//				
+//				System.out.println(Arrays.toString(tmp));
+//				System.out.println(Arrays.toString(putCopy));
+			}
 		}
-		
 	}
-}
