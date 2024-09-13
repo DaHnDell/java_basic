f55fa57a8420c8cd2aea3c109a4cd903691a1f97
DaHnDell <khanman1208@gmail.com>
2024-09-13 22:07:09 +0900

    hw

--- HomeWork/src/Hw0913/hw0913.java
+++ HomeWork/src/Hw0913/hw0913.java
@@ -14,22 +14,44 @@ public class hw0913 {
 		// 달력 출력(최소 이번 달)
 		// 이전달, 다음달, 이전해, 다음해, 특정 연 월 입력시 parse 사용해서 해당 연 월 달력 출력 
 		
+		int 
+		
 		Calendar cal = new GregorianCalendar();
 		Date da = new Date(cal.getTimeInMillis());
-		
 		da.setTime(cal.getTimeInMillis()); // 데이트 시간을 캘린더 타입
-		
 		cal.setTimeInMillis(da.getTime());; // 스트링과 스트링 타입 처럼 캘린더와 데이트 사이의 관계는 에포크를 기준으로
-		
-		int year = cal.get(Calendar.YEAR);
-		System.out.println((year));
-		int month = cal.get(Calendar.MONTH);
-		System.out.println((month));
-		int date = cal.get(Calendar.DATE);
-		System.out.println((date));
-		
-//		cal.Fir
+
 		
 		
-	}
+//		
+//		
+//		System.out.print((year));
+//		
+//		System.out.print((month));
+//		
+//		System.out.print((date));
+////		
+//		if() {
+//			
+//			string dateval[7] = {"SUN", "MON", "TUE", "WED", "THD", "FRI", "SAT"};
+//			
+//			if ((Y % 4 == 0 && Y % 100 != 0) || (Y % 4 == 0 && Y % 400 == 0))
+//			{
+//			yoon = 1;
+//			}
+//			else
+//			{
+//			yoon = 0;
+//			}
+//			
+		}
+		
 }
+
+
+void printToday() {
+	
+	
+	return;
+}
+
--- HomeWork/src/Hw0913/hw0913_02.java
+++ HomeWork/src/Hw0913/hw0913_02.java
@@ -1,8 +1,70 @@
 package Hw0913;
 
+import java.util.Arrays;
+
 public class hw0913_02 {
 	public static void main(String[] args) {
-		String str = "https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=%EA%B0%80%EB%82%98%EB%8B%A4%EB%9D%BC";
+		String str = "https://search.naver.com/search.naver?"
+				+ "where=nexearch"
+				+ "&sm=top_hty"
+				+ "&fbm=0"
+				+ "&ie=utf8"
+				+ "&query=%EA%B0%80%EB%82%98%EB%8B%A4%EB%9D%BC";
+		
+		System.out.println("프로토콜 구분자 >> ");
+		String protocolQualifier = str.substring(0, str.indexOf("://"));
+		System.out.println(protocolQualifier);
+		System.out.println();
+		System.out.println("도메인 >> ");
+		String domainQualifier = str.substring(str.indexOf("//")+2, str.lastIndexOf("/"));
+		System.out.println(domainQualifier);
+		System.out.println();
+		System.out.println("파일 네임 >> ");
+		String fileName = str.substring(str.lastIndexOf("/")+1, str.lastIndexOf("?"));
+		System.out.println(fileName);
+		System.out.println();
+		System.out.println("쿼리 스트링 >> ");
+		String querryString = str.substring(str.indexOf("?")+1, str.charAt(str.length()-1));
+		System.out.println(querryString);
+		System.out.println();
+		System.out.println("키, 밸류 짝들의 값 >> ");
+		String keyValue[] = str.substring(str.indexOf("?")+1,str.length()).split("&");
+		
+		
+		
+//		String keyValue[] = str.split("\\?");
+//		System.out.println();
+		System.out.println(Arrays.toString(keyValue));
+		
+		System.out.println("키, 밸류 짝들의 값은"+ keyValue.length + " 쌍 입니다.");
+//		
+//		String[] eachKeyVal = keyValue.split("&");;
+//		System.out.println(Arrays.toString(eachKeyVal));
+//		
+		String[] eachKey = new String[keyValue.length];
+		
+		for(int i = 0; i < keyValue.length; i++) {
+//			 eachKey = keyValue[i].split("=");
+//			 eachKey[i].replace("=", "val");
+//			 System.out.println(eachKey[i]);
+			 System.out.println("key = " + keyValue[i].replace("=", "  val  = "));
+			}
+			
+		}
+		
+//		for(int i = 0; i < keyValue.length; i++) {
+//			eachKeyVal[i] = (keyValue[i].split("=");
+//			System.out.println(keyValue[i].split("="));
+//			eachVal[i] = Arrays.toString(eachKey[i].split(","));
+			
+//			System.out.println(Arrays.toString(eachKV));
+//			System.out.println("key" + i + " > " + eachKeyVal[i][0] + " value" + i +  " > " + eachKeyVal[0][i]);
+//		}
+//		System.out.println(Arrays.toString(eachKV));
+		
+		
+		
+		
 		// 1. 프로토콜의 구분자 => ://
 		// 2. 도메인 => search.naver.com  
 		// 3. 파일네임 => search.naver (?)
@@ -13,4 +75,3 @@ public class hw0913_02 {
 		
 		
 	}
-}
