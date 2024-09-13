ed6c51c6ec84cb8f081b131bcc065c7ff2261ffa
DaHnDell <khanman1208@gmail.com>
2024-09-13 18:21:30 +0900

    숙제

--- HomeWork/src/Hw0913/hw0913.java
+++ HomeWork/src/Hw0913/hw0913.java
@@ -1,20 +1,35 @@
 package Hw0913;
 
+import static java.util.Calendar.DATE;
+import static java.util.Calendar.DAY_OF_MONTH;
+import static java.util.Calendar.ERA;
+import static java.util.Calendar.MONTH;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 public class hw0913 {
 	public static void main(String[] args) {
 		// 달력 출력(최소 이번 달)
 		// 이전달, 다음달, 이전해, 다음해, 특정 연 월 입력시 parse 사용해서 해당 연 월 달력 출력 
 		
-		// url 분석 
-		// String 클래스만 사용해서 특수문자를 기반으로 찾기
-		String str = "https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=%EA%B0%80%EB%82%98%EB%8B%A4%EB%9D%BC";
+		Calendar cal = new GregorianCalendar();
+		Date da = new Date(cal.getTimeInMillis());
 		
-		// 1. 프로토콜의 구분자 => ://
-		// 2. 도메인 => search.naver.com  
-		// 3. 파일네임 => search.naver (?)
-		// 4. 쿼리 스트링 => where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=%EA%B0%80%EB%82%98%EB%8B%A4%EB%9D%BC
-		// 5. where :: nexearch 
-		// &sm :: tio_hty
-		// 현재 위의 문자열은 5쌍의 key, value를 가지고 있습니다.
+		da.setTime(cal.getTimeInMillis()); // 데이트 시간을 캘린더 타입
+		
+		cal.setTimeInMillis(da.getTime());; // 스트링과 스트링 타입 처럼 캘린더와 데이트 사이의 관계는 에포크를 기준으로
+		
+		int year = cal.get(Calendar.YEAR);
+		System.out.println((year));
+		int month = cal.get(Calendar.MONTH);
+		System.out.println((month));
+		int date = cal.get(Calendar.DATE);
+		System.out.println((date));
+		
+//		cal.Fir
+		
+		
 	}
 }
---
+++ HomeWork/src/Hw0913/hw0913_02.java
@@ -0,0 +1,16 @@
+package Hw0913;
+
+public class hw0913_02 {
+	public static void main(String[] args) {
+		String str = "https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=%EA%B0%80%EB%82%98%EB%8B%A4%EB%9D%BC";
+		// 1. 프로토콜의 구분자 => ://
+		// 2. 도메인 => search.naver.com  
+		// 3. 파일네임 => search.naver (?)
+		// 4. 쿼리 스트링 => where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=%EA%B0%80%EB%82%98%EB%8B%A4%EB%9D%BC
+		// 5. where :: nexearch 
+		// &sm :: tio_hty
+		// 현재 위의 문자열은 5쌍의 key, value를 가지고 있습니다.
+		
+		
+	}
+}
